44953b408ee917ac3e402711ad558d549896a651
Dylan Beaudette <dylan.beaudette@gmail.com>
2016-11-03 09:44:01 -0700

    fixing paths

--- pre-and-post-processing.R
+++ pre-and-post-processing.R
@@ -6,15 +6,15 @@
 library(rgdal)
 library(sharpshootR)
 
-## pre-processing: get the data from FGB and save as SHP
+## pre-processing: get the data from FGB and save as SHP in working dir
 
 # SEKI
 x <- readOGR(dsn='l:/CA792/ca792_spatial/FG_CA792_OFFICIAL.gdb', layer='ca792_a')
-writeOGR(x, dsn='s:/Service_Center/NRCS/Archive_Dylan_Beaudette/regional-projects/join-document-ideas/CA792', layer='ca792_official', driver = 'ESRI Shapefile', overwrite_layer = TRUE)
+writeOGR(x, dsn='CA792', layer='ca792_official', driver = 'ESRI Shapefile', overwrite_layer = TRUE)
 
 # CA630
 x <- readOGR(dsn='l:/CA630/FG_CA630_OFFICIAL.gdb', layer='ca630_a')
-writeOGR(x, dsn='s:/Service_Center/NRCS/Archive_Dylan_Beaudette/regional-projects/join-document-ideas/CA630', layer='ca630_official', driver = 'ESRI Shapefile', overwrite_layer = TRUE)
+writeOGR(x, dsn='CA630', layer='ca630_official', driver = 'ESRI Shapefile', overwrite_layer = TRUE)
 
 
 ## send to PostGIS / GRASS for the hard stuff
@@ -23,34 +23,34 @@ writeOGR(x, dsn='s:/Service_Center/NRCS/Archive_Dylan_Beaudette/regional-project
 ## post-processing: generate a join decision / line segment ID
 
 # SEKI
-x <- readOGR(dsn='s:/Service_Center/NRCS/Archive_Dylan_Beaudette/regional-projects/join-document-ideas/CA792', layer = 'join_lines', stringsAsFactors = FALSE)
+x <- readOGR(dsn='CA792', layer = 'join_lines', stringsAsFactors = FALSE)
 # make a unique ID for joing decisions that should survive subsequent re-generation of the join document
 x$jd_id <- generateLineHash(x)
 # save new version to standard location
-writeOGR(x, dsn='l:/CA792/join-document', layer='join_lines', driver = 'ESRI Shapefile', overwrite_layer = TRUE)
-write.csv(x@data, file='l:/CA792/join-document/text-version.csv', row.names=FALSE)
+writeOGR(x, dsn='L:/CA792/join-document', layer='join_lines', driver = 'ESRI Shapefile', overwrite_layer = TRUE)
+write.csv(x@data, file='L:/CA792/join-document/text-version.csv', row.names=FALSE)
 
 # make network diagram:
 a <- joinAdjacency(x)
 
-pdf(file='l:/CA792/join-document/network-diagram.pdf', width = 12, height = 12)
+pdf(file='L:/CA792/join-document/network-diagram.pdf', width = 12, height = 12)
 par(mar=c(0,0,0,0))
 plotSoilRelationGraph(a, spanning.tree='max', edge.scaling.factor=1, vertex.scaling.factor = 2, edge.transparency = 0)
 dev.off()
 
 
 # CA630
-x <- readOGR(dsn='l:/CA630/join-document', layer = 'ca630_join_lines', stringsAsFactors = FALSE)
+x <- readOGR(dsn='L:/CA630/join-document', layer = 'ca630_join_lines', stringsAsFactors = FALSE)
 # make a unique ID for joing decisions that should survive subsequent re-generation of the join document
 x$jd_id <- generateLineHash(x)
 # save new version to standard location
-writeOGR(x, dsn='l:/CA630/join-document', layer='join_lines', driver = 'ESRI Shapefile', overwrite_layer = TRUE)
-write.csv(x@data, file='l:/CA630/join-document/text-version.csv', row.names=FALSE)
+writeOGR(x, dsn='L:/CA630/join-document', layer='join_lines', driver = 'ESRI Shapefile', overwrite_layer = TRUE)
+write.csv(x@data, file='L:/CA630/join-document/text-version.csv', row.names=FALSE)
 
 # make network diagram:
 a <- joinAdjacency(x)
 
-pdf(file='l:/CA630/join-document/network-diagram.pdf', width = 12, height = 12)
+pdf(file='L:/CA630/join-document/network-diagram.pdf', width = 12, height = 12)
 par(mar=c(0,0,0,0))
 plotSoilRelationGraph(a, spanning.tree='max', edge.scaling.factor=1, vertex.scaling.factor = 2, edge.transparency = 0)
 dev.off()
